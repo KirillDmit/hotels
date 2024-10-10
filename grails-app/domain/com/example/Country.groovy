4f11942936719d9bc1058b8e22b90e4c248301a1
KirillDmit <kirilldmitriev20015@gmail.com>
2024-10-10 23:54:04 +0500

    Cascade del
    Cascade delete Country Hotel

--- grails-app/domain/com/example/Country.groovy
+++ grails-app/domain/com/example/Country.groovy
@@ -4,9 +4,15 @@ class Country {
     String name
     String capital
 
+    static hasMany = [hotels: Hotel]
+
     static constraints = {
         name maxSize: 255, unique: true, nullable: false
         capital maxSize: 128, nullable: false
+    }
+
+    static mapping = {
+        hotels cascade: 'all-delete-orphan'
     }
 
     String toString() {
