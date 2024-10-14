0676ddab3dabe8311c1041c2d314f25cb44e1af5
KirillDmit <kirilldmitriev20015@gmail.com>
2024-10-14 17:02:28 +0500

    add data domain

--- grails-app/init/com/example/BootStrap.groovy
+++ grails-app/init/com/example/BootStrap.groovy
@@ -9,12 +9,17 @@ class BootStrap {
         def russia = new Country(name: 'Россия', capital: 'Москва').save(failOnError: true)
         def usa = new Country(name: 'США', capital: 'Вашингтон').save(failOnError: true)
         def germany = new Country(name: 'Германия', capital: 'Берлин').save(failOnError: true)
+        def france = new Country(name: 'Франция', capital: 'Париж').save(failOnError: true)
 
-        new Country(name: 'Франция', capital: 'Париж').save(failOnError: true)
+        new Country(name: 'Италия', capital: 'Рим').save(failOnError: true)
+        new Country(name: 'Испания', capital: 'Мадрид').save(failOnError: true)
+
         new Hotel(name: 'Звезды Арбата', country: russia, stars: 5, website: 'https://arbatstars.activhotels.ru/').save(failOnError: true)
         new Hotel(name: 'Отель Санкт-Петербург', country: russia, stars: 4).save(failOnError: true)
         new Hotel(name: 'New York Hotel', country: usa, stars: 5, website: 'https://newyorkhotel.com').save(failOnError: true)
         new Hotel(name: 'Berlin Hotel', country: germany, stars: 4).save(failOnError: true)
+        new Hotel(name: 'Ritz Paris', country: france, stars: 5, website: 'https://www.ritzparis.com').save(failOnError: true)
+        new Hotel(name: 'Le Meurice', country: france, stars: 5, website: 'https://www.dorchestercollection.com/paris/le-meurice').save(failOnError: true)
     }
 
     def destroy = {
